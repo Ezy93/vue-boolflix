20a8ccdfe90f32716e749d7664db6f53e1ca476d
Ezy93 <giordani001@gmail.com>
2022-04-01 18:32:31 +0200

    inserita funzione che permette di creare grazie all'input dell'utente un data da inserire nella query della chiamata api

--- src/components/IndexHeader.vue
+++ src/components/IndexHeader.vue
@@ -1,15 +1,47 @@
 <template>
     <header>
-        
+        <div class="row">
+            <div class="col-12">
+                <input type="text" v-model="searchedString">
+                
+                <div class="btn btn-primary" @click="stringModifier">
+                    cerca
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 
 <script>
 export default {
-    name: "IndexHeader"
+    name: "IndexHeader",
+    data: function(){
+        return{
+            searchedString: null,
+            queryString: null,
+        }
+    },
+    created: function(){
+        
+        
+        
+    },
+    methods:{
+        stringModifier(){
+            console.log(this.searchedString.split(" "))
+            this.queryString =  this.searchedString.split(" ").join("+")
+            console.log(this.queryString)
+        },
+        
+        
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-
+/* stilizzazione di prova da eliminare */
+    div.prova{
+        height: 100px;
+        width: 200px;
+    }
 </style>
--- src/components/IndexMain.vue
+++ src/components/IndexMain.vue
@@ -5,9 +5,24 @@
 </template>
 
 <script>
+/* import axios from 'axios'; */
+
 export default {
-    name: "IndexMain"
+    name: "IndexMain",
+    data: function(){
+        return{
+            filmsList: null,
+            seriesList: null,
+        }
+    },
+    created: function(){
+        
+    },
+    methods:{
+        
+    },
 }
+
 </script>
 
 <style lang="scss" scoped>
