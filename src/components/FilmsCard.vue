385ce635e655e41e55a64549a85244c91e0d9965
Ezy93 <giordani001@gmail.com>
2022-04-04 19:28:28 +0200

    costruzione dei poster senza hover completata corretto errore con il path dell'immagine giusta

--- src/components/FilmsCard.vue
+++ src/components/FilmsCard.vue
@@ -1,16 +1,39 @@
 <template>
     <div class="d-flex flex-wrap container">
-        <div v-for="(element) in films" :key="element.id" class="my-card mx-2 ">
-            <img class="poster img-fluid " v-if="(element.backdrop_path !== null)" :src="`https://image.tmdb.org/t/p/w342${element.backdrop_path}`" :alt="element.title">
-            <p v-else class="bg-dark h-100 text-white py-5 text-center">poster non disponibile</p>
-            <div class="my-description">
+        <div v-for="(element) in films" :key="element.id" class="my-card mx-2 mb-2 position-relative">
+            <img class="my-poster h-100 w-100 " v-if="(element.backdrop_path !== null)" :src="`https://image.tmdb.org/t/p/w342${element.poster_path}`" :alt="element.title">
+            <p v-else class="bg-dark  text-white text-center">poster non disponibile</p>
+            <div class="my-description px-2 my-5 position-relative text-white">
                 <div>
-                    <!-- finire di sistemare le card in ogni div e come se fossero i vecchi li -->
+                    Titolo: {{element.title}}
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>
+                    Titolo originale: {{element.original_title}}
+                </div>
+                <div>
+                    Lingua: <img class="my-flag" :src="`https://www.unknown.nu/flags/images/${element.original_language}-100`" :alt="`${element.original_language}`">
+                </div>
+                <div class="d-flex align-items-center">
+                    
+                    <div v-if="(newVoteRange(element.vote_average) < 0)">
+                        {{newVoteRange(element.vote_average)}} 
+                    </div>
+                    <div v-if="(newVoteRange(element.vote_average) >= 0)">
+                        vote:
+                    </div>
+                    <div class="ms-1">
+                        <font-awesome-icon icon="fa-solid fa-star"  :class="{active: (newVoteRange(element.vote_average) > 0)}"/>
+                        <font-awesome-icon icon="fa-solid fa-star"  :class="{active: (newVoteRange(element.vote_average) > 1)}"/>
+                        <font-awesome-icon icon="fa-solid fa-star"  :class="{active: (newVoteRange(element.vote_average) > 2)}"/>
+                        <font-awesome-icon icon="fa-solid fa-star"  :class="{active: (newVoteRange(element.vote_average) > 3)}"/>
+                        <font-awesome-icon icon="fa-solid fa-star"  :class="{active: (newVoteRange(element.vote_average) > 4)}"/>
+                        
+                    </div>
+            
+                </div>
+                <div class="mt-1">
+                    {{element.overview}}
+                </div>
             </div>
         </div>
         <!-- <ul v-for="(element) in films" :key="element.id" class="mx-2 p-2">
@@ -87,25 +110,44 @@ div{
         border: 2px solid black;
         list-style-type: none;
         width: calc(100% / 5 - 1rem);
+        height: 23rem;
+
+        img.my-poster{
+            position: absolute;
+            object-fit: cover;
+            height: 100%;
+        }
+
+        
+
+        div.my-description{
+            
+            div.mt-1{
+                overflow-y: auto;
+                height: 10rem;
+            }
+        }
+        
+        
+        img.my-flag{
+            width: 24px;
+            height: 24px;
+            object-fit: contain;
+        }
     }
+        .active{
+            color: #ffbd00;
+        } 
     /* ul{
-            border: 2px solid black;
+        border: 2px solid black;
             list-style-type: none;
             width: calc(100% / 5 - 1rem);
             li{
                 padding: .2rem;
-                img.my-flag{
-                    width: 24px;
-                    height: 24px;
-                    object-fit: contain;
-                }
                 img.poster{
                     width: 100%;
                 }
             }
-        }
-        .active{
-            color: #ffbd00;
-        } */
+        }*/
 }
 </style>
