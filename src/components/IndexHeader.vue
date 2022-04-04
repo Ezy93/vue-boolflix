87e1535dc1ea7546883f8f3a6f2d2889d71d8a38
Ezy93 <giordani001@gmail.com>
2022-04-04 12:33:59 +0200

    corretto errore nella comparazione della stringa cercata per far apparire l'easter egg

--- src/components/IndexHeader.vue
+++ src/components/IndexHeader.vue
@@ -1,12 +1,15 @@
 <template>
     <header>
         <div class="row">
-            <div class="col-12">
-                <!-- //! RICORDARSI DELL'ERRORE CHE DA QUANDO SI PROVA A FARE UNA RICERCA CON IMPUT VUOTO -->
-                <input type="text" v-model="searchedString" @keyup.enter="stringModifier">
+            <div class="col-12 d-flex">
+                <div>
+                    <input type="text" v-model="searchedString" @keyup.enter="stringModifier">
+                </div>
                 
-                <div class="btn btn-primary" @click="stringModifier">
-                    cerca
+                <div>
+                    <div class="btn btn-primary" @click="stringModifier">
+                        cerca
+                    </div>
                 </div>
             </div>
         </div>
@@ -18,7 +21,7 @@ export default {
     name: "IndexHeader",
     data: function(){
         return{
-            searchedString: null,
+            searchedString: '',
             queryString: null,
         }
     },
@@ -30,8 +33,9 @@ export default {
     },
     methods:{
         stringModifier(){
-            if(this.searchedString === null){
+            if(this.searchedString === null || this.searchedString.trim().length === 0){
                 this.searchedString = "into the void"
+                
             }
             this.queryString =  this.searchedString.trim().split(" ").join("+")
             console.log(this.queryString)
